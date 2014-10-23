688424f46722920b7235b85c8f8d117fcda82728
iGuest <vivyanw@uw.edu>
2014-10-23 09:42:50 -0700

    updated signup.js

--- js/signup.js
+++ js/signup.js
@@ -3,9 +3,28 @@
     This script will load the state select list and validate the form before submission
 */
 
-document.addEventListener('DOMContentLoaded', function(){
-	
-});
+"use strict";
+
+document.addEventListener('DOMContentLoaded', onReady);
+
+function onReady() {
+
+	var elem = document.getElementById("state");
+	for (var i = 0; i < usStates.length; i++) {
+		var opt = document.createElement("option");
+		opt.value = usStates[i].code;
+		var optText = document.createTextNode(usStates[i].name);
+		opt.appendChild(optText);
+		elem.appendChild(opt);
+	}
+}
+
 
 var option = document.createElement("OPTION");
-option.value = document.createElement();
+option.value = document.createElement(states.code);
+
+document.addEventListener('onCancelButtonClick', function(){
+	alert('Are you sure you want to leave this page?');
+
+	window.loaction = "http://www.google.com";
+})
