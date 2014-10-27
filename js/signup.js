e8f1b4d25c707b9454d7c57964dc9a9ec2cc7392
Vivyan Woods <dr.v.woods@gmail.com>
2014-10-26 21:29:38 -0700

    added validation
    started working on validating the form.  still working on occupation
    other

--- js/signup.js
+++ js/signup.js
@@ -21,17 +21,38 @@ function onReady() {
 		elem.appendChild(opt);
 	}
 
-	cancelButton.addEventListener('click', cancelForm)
+	occupation.addEventListener('change', occupationOptions(thisForm));
+	cancelButton.addEventListener('click', cancelForm);
+	thisForm.addEventListener('submit', onSubmit)
 }
 
-
-var option = document.createElement("OPTION");
-option.value = document.createElement(states.code);
+function occupationOptions (form) {
+	
+	//form.getElementById("occupationOther").style.display = inline;
+}
 
 function cancelForm() {
 	var response = window.confirm('Are you sure you want to leave this page?');
 
-	if (response) {
-		window.loaction = "http://www.google.com";
+	if (response != 0) {
+		window.loaction = 'http://www.google.com';
 	}
+}
+
+function onSubmit (event) {
+	// body...
+}
+
+function validate(form) {
+	//
+	var necessaryFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+	necessaryFields.forEach(validateField, form);
+}
+
+function validateField (field) {
+	if (0 == this[field].value.trim().length) {
+		this[field].className = 'form-control invalid'
+		return false;
+	} 
+	return true;
 }
